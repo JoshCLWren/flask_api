a22e0843c5945214ccb7aae4c6b7b150df491e9f
Joshisplutar <joshisplutar@gmail.com>
2020-08-24 21:42:00 -0500

    Add jwt auth implemented jwt token auth and add login template

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask import Flask, jsonify, abort, make_response, request, url_for, session, flash, render_template
+from functools import wraps
+import jwt
+import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'JustDemonstrating'
+
+def check_for_token(func):
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        token = request.args.get('token')
+        if not token:
+            return jsonify({'message': 'Missing token'}), 403
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except:
+            return jsonify({'message': 'Invalid Token'}), 403
+        return func(*args, **kwargs)
+    return wrapped
 
 tasks = [
     {
@@ -44,6 +61,7 @@ def create_task():
     }
     tasks.append(task)
     return jsonify({'task': task}), 201
+
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
@@ -81,7 +99,32 @@ def make_public_task(task):
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+
+@app.route('/public')
+def public():
+    return 'anyone can view this'
+
+@app.route('/auth')
+@check_for_token
+def authorised():
+    return 'this is only viewable with a token'
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.form['username'] and request.form['password'] == 'passsword':
+        session['logged_in'] = True
+        token = jwt.encode({
+            'user': request.form['username'],
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=600)
+        },
+        app.config['SECRET_KEY'])
+        return get_tasks()
+    else:
+        return make_response('Unable to verify', 403, {'WWW-Authenticate': 'Basic Realm: "Login Now"'})
 
 if __name__ == '__main__':
     app.run(debug=True)
